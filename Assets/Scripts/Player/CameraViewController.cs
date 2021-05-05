399d21da7ef876e5be4344f04731a689cee0a6f0
Skill-For-Kill <nice.partos@gmail.com>
2021-05-05 17:23:32 +0300

    move scripts

---
+++ Assets/Scripts/Player/CameraViewController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraViewController : MonoBehaviour
+{
+    public float mouseXSensitivity = 50f;
+    public float mouseYSensitivity = 50f;
+
+    public Transform playerTransform;
+
+    private float xRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * 10 * mouseXSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * 10 * mouseYSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerTransform.Rotate(Vector3.up * mouseX);
+    }
+}
