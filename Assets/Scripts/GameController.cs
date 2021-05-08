3b24b08b1d1fe89350d0ddebe14a72b9c41bead5
Skill-For-Kill <nice.partos@gmail.com>
2021-05-08 12:56:06 +0300

    fix photon movement

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -16,7 +16,15 @@ public class GameController : MonoBehaviour
     private void CreatePlayer()
     {
         Debug.Log("Creating Player");
-        PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), new Vector3(0, 0, 0), Quaternion.identity);
+        GameObject player = PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PhotonPlayer"), new Vector3(0, 0, 0), Quaternion.identity);
+        var characterController = player.GetComponent<CharacterController>();
+        var photonView = player.GetComponent<PhotonView>();
+        var camera = player.GetComponentInChildren<Camera>();
+        if (photonView.IsMine)
+        {
+            characterController.enabled = true;
+            camera.enabled = true;
+        }
     }
     
     // Update is called once per frame
